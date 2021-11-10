fbdbf06ffcc8ea03a94e6af3e9148a54932b0796
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-11-10 14:23:07 -0500

    linting

--- danesfield/core/management/commands/populate_dev_data.py
+++ danesfield/core/management/commands/populate_dev_data.py
@@ -21,7 +21,7 @@ test_filenames = ['tileset.json', 'dragon_high.b3dm', 'dragon_low.b3dm', 'dragon
 def populate_dev_data():
     point_cloud_file = ChecksumFile.objects.create(
         name='TilesetWithDiscreteLOD.txt',
-        file=ContentFile('i\'m a point cloud (not really)').read(),
+        file=ContentFile("i'm a point cloud (not really)").read(),
     )
     dataset: Dataset = Dataset.objects.create(
         name='TilesetWithDiscreteLOD',
