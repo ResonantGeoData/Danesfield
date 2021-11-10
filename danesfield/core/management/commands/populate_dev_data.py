852ce93f5aecbb92e8a72abb0d06408d50b174bf
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-11-10 16:45:40 -0500

    Actually upload dummy point cloud, add logging

--- danesfield/core/management/commands/populate_dev_data.py
+++ danesfield/core/management/commands/populate_dev_data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from django.core.files import File
-from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import SimpleUploadedFile
 import djclick as click
 from rgd.models.common import ChecksumFile
 
@@ -19,20 +19,27 @@ test_filenames = ['tileset.json', 'dragon_high.b3dm', 'dragon_low.b3dm', 'dragon
 
 @click.command()
 def populate_dev_data():
-    point_cloud_file = ChecksumFile.objects.create(
-        name='TilesetWithDiscreteLOD.las',
-        file=ContentFile("i'm a point cloud (not really)").read(),
-    )
-    dataset: Dataset = Dataset.objects.create(
-        name='TilesetWithDiscreteLOD',
-        imageless=True,
-        point_cloud_file=point_cloud_file,
-    )
+    if Dataset.objects.filter(name='TilesetWithDiscreteLOD').exists():
+        dataset: Dataset = Dataset.objects.get(name='TilesetWithDiscreteLOD')
+        click.echo(f'Dataset "TilesetWithDiscreteLOD" already exists (pk = {dataset.pk}).')
+    else:
+        point_cloud_file = ChecksumFile.objects.create(
+            name='TilesetWithDiscreteLOD.las',
+            file=SimpleUploadedFile(name='TilesetWithDiscreteLOD.las', content=b'A' * 10),
+        )
+        dataset: Dataset = Dataset.objects.create(
+            name='TilesetWithDiscreteLOD',
+            imageless=True,
+            point_cloud_file=point_cloud_file,
+        )
+        click.echo(f'Created dataset "TilesetWithDiscreteLOD" (pk = {dataset.pk})')
     dataset_run: DatasetRun = DatasetRun.objects.create(
         dataset=dataset, status=DatasetRun.Status.SUCCEEDED, output_log='test'
     )
+    click.echo(f'Created DatasetRun for Dataset {dataset.pk}.')
     for filename in test_filenames:
         with open(TEST_DATA_DIRECTORY / filename, 'rb') as fd:
             dataset_run.output_files.add(
                 ChecksumFile.objects.create(name=f'tiler/{filename}', file=File(fd))
             )
+            click.echo(f'  Added ChecksumFile "tiler/{filename}" to DatasetRun output.')
