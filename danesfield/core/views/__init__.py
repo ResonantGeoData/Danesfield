b9b34e4f23ab19b87665968f3bbbbe4011e08406
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-12-13 13:14:24 -0500

    Add endpoint for retrieving Danesfield Algorithm

--- danesfield/core/views/__init__.py
+++ danesfield/core/views/__init__.py
@@ -1,13 +1,26 @@
 from drf_yasg.utils import swagger_auto_schema
-from rdoasis.algorithms.views.serializers import AlgorithmRunSerializer, AlgorithmTaskSerializer
+from rdoasis.algorithms.views.serializers import (
+    AlgorithmRunSerializer,
+    AlgorithmSerializer,
+    AlgorithmTaskSerializer,
+)
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
 from danesfield.core.tasks import run_danesfield
+from danesfield.core.utils import danesfield_algorithm
 
 
 class DanesfieldAlgorithmViewSet(ViewSet):
+    @swagger_auto_schema(method='GET')
+    @action(detail=False, methods=['GET'])
+    def algorithm(self, request):
+        """Return the singleton Danesfield algorithm."""
+
+        alg = danesfield_algorithm()
+        return Response(AlgorithmSerializer(alg).data)
+
     @swagger_auto_schema(method='POST', request_body=AlgorithmRunSerializer())
     @action(detail=False, methods=['POST'])
     def run(self, request):
