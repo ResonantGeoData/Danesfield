883e9dc2770bbc588c9e4f8dd57396a05de212a4
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-01-03 20:07:55 -0500

    Add file browser to focus page

---
+++ client/src/components/FileBrowser.vue
@@ -0,0 +1,153 @@
+<template>
+  <v-card
+    height="100%"
+    outlined
+  >
+    <v-card-title>
+      <router-link
+        :to="{ name: 'focus', query: { location: rootDirectory } }"
+        style="text-decoration: none;"
+        class="mx-2"
+      >
+        {{ '.' }}
+      </router-link>
+      <template v-for="(part, i) in splitLocation">
+        /
+        <template v-if="part">
+          <router-link
+            :key="part"
+            :to="{ name: 'focus', query: { location: locationSlice(i) } }"
+            style="text-decoration: none;"
+            class="mx-2"
+          >
+            {{ part }}
+          </router-link>
+        </template>
+      </template>
+    </v-card-title>
+    <v-progress-linear
+      v-if="updating"
+      indeterminate
+    />
+    <v-divider v-else />
+    <v-list>
+      <v-list-item
+        v-for="item in items"
+        :key="item.path"
+        color="primary"
+        @click="selectPath(item)"
+      >
+        <v-icon
+          class="mr-2"
+          color="primary"
+        >
+          <template v-if="item.isFolder">
+            mdi-folder
+          </template>
+          <template v-else>
+            mdi-file
+          </template>
+        </v-icon>
+        {{ item.path }}
+        <v-spacer />
+
+        <v-list-item-action v-if="!item.isFolder">
+          <v-btn
+            icon
+            :href="item.file"
+          >
+            <v-icon color="primary">
+              mdi-download
+            </v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script lang="ts">
+import {
+  computed, defineComponent, Ref, ref, watch,
+} from '@vue/composition-api';
+import { axiosInstance } from '@/api';
+import { RawLocation } from 'vue-router';
+import { ChecksumFile } from '@/types';
+
+const parentDirectory = '..';
+const rootDirectory = '';
+
+interface FileItem extends ChecksumFile {
+  isFolder: boolean;
+  path: string;
+}
+
+export default defineComponent({
+  name: 'FileBrowser',
+  setup(props, ctx) {
+    const location = ref(rootDirectory);
+    const items: Ref<FileItem[]> = ref([]);
+    const updating = ref(false);
+
+    const splitLocation = computed(() => location.value.split('/'));
+
+    watch(location, async () => {
+      updating.value = true;
+
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      const { data } = await axiosInstance.get('/rgd/checksum_file/tree', { params: { path_prefix: location.value } });
+
+      items.value = [
+        ...location.value !== rootDirectory ? [{ path: parentDirectory, isFolder: true }] : [],
+        ...[
+          ...Object.keys(data.files).map((name: string) => (
+            { isFolder: false, path: name, ...data.files[name] }
+          )),
+          ...Object.keys(data.folders).map((name: string) => (
+            { isFolder: true, path: `${name}/`, ...data.files[name] }
+          )),
+        ],
+      ];
+      updating.value = false;
+    }, { immediate: true });
+
+    watch(location, () => {
+      const { location: existingLocation } = ctx.root.$route.query;
+
+      const route = {
+        ...ctx.root.$route,
+        query: { location: location.value },
+      } as RawLocation;
+
+      // Update route when location changes
+      if (existingLocation === location.value) { return; }
+      ctx.root.$router.push(route);
+    });
+
+    function locationSlice(index: number) {
+      return `${splitLocation.value.slice(0, index + 1).join('/')}/`;
+    }
+
+    function selectPath(item: FileItem) {
+      const { path, isFolder } = item;
+
+      if (!isFolder) { return; }
+      if (path === parentDirectory) {
+        const slicedLocation = location.value.split('/').slice(0, -2);
+        location.value = slicedLocation.length ? `${slicedLocation.join('/')}/` : '';
+      } else {
+        location.value = `${location.value}${path}`;
+      }
+    }
+
+    return {
+      items,
+      splitLocation,
+      updating,
+      rootDirectory,
+      locationSlice,
+      selectPath,
+    };
+  },
+});
+</script>
--- client/src/views/Focus.vue
+++ client/src/views/Focus.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    TODO: Focus Tab
-  </div>
+  <v-row
+    style="height: 100%"
+    no-gutters
+  >
+    <v-col cols="2">
+      <file-browser />
+    </v-col>
+    <v-col cols="10">
+      <!-- TODO: put iframe to RGD server-rendered pages here -->
+    </v-col>
+  </v-row>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import FileBrowser from '@/components/FileBrowser.vue';
 
 export default defineComponent({
-  setup() {
-
+  name: 'Focus',
+  components: { FileBrowser },
+  props: {
+    datasetId: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+    };
   },
 });
 </script>
