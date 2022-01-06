c81f0e6a75e8248c300aadff6a255b198716e00b
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-01-06 15:56:43 -0500

    Combine Explore + Focus, list datasets When a dataset id is not given in the url, a list of datasets is presented instead of the file browser

---
+++ client/src/components/DatasetList.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-list
+    outlined
+    height="100%"
+  >
+    <v-card
+      v-for="dataset in datasets"
+      :key="dataset.id"
+    >
+      <v-list-item @click="viewDataset(dataset.id)">
+        <v-list-item-content>
+          <v-list-item-title>{{ dataset.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </v-list>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+import { axiosInstance } from '@/api';
+import router from '@/router';
+import { AxiosResponse } from 'axios';
+import { RawLocation } from 'vue-router';
+
+export default defineComponent({
+  setup() {
+    const datasets = ref([]);
+
+    axiosInstance.get('/datasets/')
+      .then((resp: AxiosResponse) => {
+        datasets.value = resp.data.results;
+      });
+
+    function viewDataset(datasetId: string) {
+      const route = {
+        name: 'explore',
+        params: { datasetId },
+        query: {
+          location: '',
+        },
+      } as RawLocation;
+
+      router.push(route);
+    }
+
+    return {
+      datasets,
+      viewDataset,
+    };
+  },
+});
+</script>
