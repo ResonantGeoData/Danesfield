034a3577375840eba1deb99ce993eea46a125a04
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-01-20 18:30:13 -0500

    Refactor DatasetViewSet viewer action code

--- danesfield/core/views/dataset.py
+++ danesfield/core/views/dataset.py
@@ -16,29 +16,31 @@ class DatasetViewSet(BaseDatasetViewSet):
         """Redirects to the appropriate viewer for the given file."""
         dataset: Dataset = get_object_or_404(Dataset, pk=pk)
         checksum_file: ChecksumFile = get_object_or_404(dataset.files, name=path)
-        for model in [Image, Mesh3D, Tiles3D]:
-            try:
-                if model == Tiles3D:
-                    if checksum_file.file_set is None:
-                        continue
-                    json_file = checksum_file.file_set.files.filter(
-                        name__endswith='tileset.json'
-                    ).first()
-                    ingested_file = model.objects.get(json_file=json_file)
-                else:
-                    ingested_file = model.objects.get(file=checksum_file)
 
-                    # Display the associated Raster if this is an Image
-                    if model == Image:
-                        ingested_file: RasterMeta = (
-                            ingested_file.imageset_set.first().raster.rastermeta
-                        )
-                        model = RasterMeta
+        # Try Image model
+        ingested_file = Image.objects.filter(file=checksum_file).first()
+        if ingested_file is not None:
+            raster_meta = ingested_file.imageset_set.first().raster.rastermeta
+            return HttpResponseRedirect(
+                reverse(RasterMeta.detail_view_name, kwargs={'pk': raster_meta.pk})
+            )
 
-                return HttpResponseRedirect(
-                    reverse(model.detail_view_name, kwargs={'pk': ingested_file.pk})
-                )
-            except model.DoesNotExist:
-                pass
+        # Try Mesh3D model
+        ingested_file = Mesh3D.objects.filter(file=checksum_file).first()
+        if ingested_file is not None:
+            return HttpResponseRedirect(
+                reverse(Mesh3D.detail_view_name, kwargs={'pk': ingested_file.pk})
+            )
 
+        # Try Tiles3D model
+        if checksum_file.file_set is not None:
+            tileset = checksum_file.file_set.files.filter(name__endswith='tileset.json').first()
+            if tileset is not None:
+                ingested_file = Tiles3D.objects.filter(json_file=tileset).first()
+                if ingested_file is not None:
+                    return HttpResponseRedirect(
+                        reverse(Tiles3D.detail_view_name, kwargs={'pk': ingested_file.pk})
+                    )
+
+        # No match found
         return HttpResponseNotFound()
