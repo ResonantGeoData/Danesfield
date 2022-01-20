f78daf30c4347c047db95485ea22cc5e69089064
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-01-20 17:02:40 -0500

    Add 3D tiles to viewer redirect endpoint

--- danesfield/core/views/dataset.py
+++ danesfield/core/views/dataset.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.request import Request
 from rgd.models import ChecksumFile
-from rgd_3d.models import Mesh3D
+from rgd_3d.models import Mesh3D, Tiles3D
 from rgd_imagery.models import Image, RasterMeta
 
 
@@ -16,14 +16,24 @@ class DatasetViewSet(BaseDatasetViewSet):
         """Redirects to the appropriate viewer for the given file."""
         dataset: Dataset = get_object_or_404(Dataset, pk=pk)
         checksum_file: ChecksumFile = get_object_or_404(dataset.files, name=path)
-        for model in [Image, Mesh3D]:
+        for model in [Image, Mesh3D, Tiles3D]:
             try:
-                ingested_file = model.objects.get(file=checksum_file)
+                if model == Tiles3D:
+                    if checksum_file.file_set is None:
+                        continue
+                    json_file = checksum_file.file_set.files.filter(
+                        name__endswith='tileset.json'
+                    ).first()
+                    ingested_file = model.objects.get(json_file=json_file)
+                else:
+                    ingested_file = model.objects.get(file=checksum_file)
 
-                # Display the associated Raster if this is an Image
-                if model == Image:
-                    ingested_file: RasterMeta = ingested_file.imageset_set.first().raster.rastermeta
-                    model = RasterMeta
+                    # Display the associated Raster if this is an Image
+                    if model == Image:
+                        ingested_file: RasterMeta = (
+                            ingested_file.imageset_set.first().raster.rastermeta
+                        )
+                        model = RasterMeta
 
                 return HttpResponseRedirect(
                     reverse(model.detail_view_name, kwargs={'pk': ingested_file.pk})
