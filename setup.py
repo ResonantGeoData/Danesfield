1ea8ef7d714d3b18a3f4cfce98fb8ca96829094d
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-03-03 19:03:01 -0500

    Add content security policy needed for iframes

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'django-allauth',
         'django-click',
         'django-configurations[database,email]',
+        'django-csp',
         'django-extensions',
         'django-filter',
         'django-oauth-toolkit',
