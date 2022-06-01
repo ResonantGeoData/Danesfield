c45fa7289e2fbcd7e0b77fd022566ff6774d9dcb
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-06-01 19:51:39 -0400

    Simplify test fixtures Use RGD's built-in test data instead of our own

--- danesfield/core/tests/conftest.py
+++ danesfield/core/tests/conftest.py
@@ -1,17 +1,17 @@
-from typing import List
+from __future__ import annotations
 
-from factory.django import FileField
 import pytest
 from pytest_factoryboy import register
 from rdoasis.algorithms.tests.factories import DatasetFactory
 from rest_framework.test import APIClient
-from rgd.models import ChecksumFile
-from rgd_3d.management.commands.rgd_3d_demo import load_tiles_3d_files
+from rgd_3d.management.commands.rgd_3d_demo import load_mesh_3d_files, load_tiles_3d_files
 from rgd_3d.models import Mesh3D, Tiles3D
 from rgd_3d.tasks.jobs import task_read_3d_tiles_file
-from rgd_imagery.models import Image, ImageSet, Raster
+from rgd_imagery.management.commands._data_helper import load_raster_files, make_raster_dict
+from rgd_imagery.models import Raster
+from rgd_imagery.tasks.jobs import task_populate_raster
 
-from .factories import DATA_DIR, ChecksumFileFactory, UserFactory
+from .factories import ChecksumFileFactory, UserFactory
 
 
 @pytest.fixture
@@ -35,46 +35,20 @@ def admin_api_client(user_factory) -> APIClient:
 
 
 @pytest.fixture
-def sample_output_image():
-    """Sample output image from the Danesfield algorithm."""
-    file_path = DATA_DIR / 'images' / 'threshold_CLS.tif'
-    return ChecksumFileFactory(name=file_path, file=FileField(from_path=(file_path)))
-
-
-@pytest.fixture(params=['44_building_49.obj', 'building_49.ply'])
-def sample_output_mesh(request):
-    """Sample output mesh from the Danesfield algorithm."""
-    file = str(DATA_DIR / 'meshes' / request.param)
-    return ChecksumFileFactory(name=file, file=FileField(from_path=(file)))
+def raster() -> Raster:
+    pk = load_raster_files([make_raster_dict(['paris_france_10.tiff'])])[0]
+    task_populate_raster(pk)
+    return Raster.objects.get(pk=pk)
 
 
 @pytest.fixture
-def sample_output_3d_tiles() -> List[ChecksumFile]:
-    """Sample 3D tiles output from the Danesfield algorithm."""
-    return [
-        ChecksumFileFactory(name=file_path, file=FileField(from_path=(file_path)))
-        for file_path in (DATA_DIR / '3d_tiles').iterdir()
-        if not file_path.is_dir()
-    ]
+def mesh() -> Mesh3D:
+    pk = load_mesh_3d_files(['topo.vtk'])[0]
+    return Mesh3D.objects.get(pk=pk)
 
 
 @pytest.fixture
-def raster(sample_output_image):
-    image = Image.objects.create(file=sample_output_image)
-    image_set = ImageSet.objects.create(name=image.file.name)
-    image_set.images.set([image])
-    raster: Raster = Raster.objects.create(name=image.file.name, image_set=image_set)
-    raster._run_tasks()
-    return raster
-
-
-@pytest.fixture
-def mesh(sample_output_mesh):
-    return Mesh3D.objects.create(file=sample_output_mesh)
-
-
-@pytest.fixture
-def tiles3d(sample_output_3d_tiles):
+def tiles3d() -> Tiles3D:
     pk = load_tiles_3d_files(['jacksonville-textured.zip'])[0]
     task_read_3d_tiles_file(pk)
     return Tiles3D.objects.get(pk=pk)
