3db32602dd315afd1e0804426585c1db60cfdf13
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-06-02 17:13:25 -0400

    fix typo

--- danesfield/core/views/serializers.py
+++ danesfield/core/views/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 class DatasetListQueryParamsSerializer(serializers.Serializer):
     include_input_datasets = serializers.BooleanField(
-        default=True, help_text='Whether or not to include input datasets.'
+        default=True, help_text='Whether or not to include input datasets in response.'
     )
     include_output_datasets = serializers.BooleanField(
-        default=True, help_text='Whether or not to include input datasets.'
+        default=True, help_text='Whether or not to include output datasets in response.'
     )
