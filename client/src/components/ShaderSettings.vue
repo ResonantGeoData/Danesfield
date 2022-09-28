3e6993eca64ced7c133f2936e140b5ed50d600e1
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-09-28 14:07:12 -0400

    Add code from demo

--- client/src/components/ShaderSettings.vue
+++ client/src/components/ShaderSettings.vue
@@ -2,6 +2,7 @@
 import {
   ref, PropType, watchEffect, computed,
 } from 'vue';
+import type { ComputedRef } from 'vue';
 import { createShader } from '@/utils/cesium';
 import { axiosInstance } from '@/api';
 import type { Tiles3DMeta } from '@/types';
@@ -54,22 +55,55 @@ function createShaderOption(
   };
 }
 
-const shaderOptions: {
+const shaderOptions: ComputedRef<{
   title: string;
   propertyName?: string;
   sourceMin: number;
   sourceMax: number;
-}[] = [
-  createShaderOption('Default', undefined, 0.0, 1.0),
-  createShaderOption('C 0_0', 'c0_0', 0.043, 0.181),
-  createShaderOption('C 1_0', 'c1_0', -0.036, 0.196),
-  createShaderOption('C 1_1', 'c1_1', 0.074, 0.324),
-  createShaderOption('C 2_0', 'c2_0', -0.072, 0.897),
-  createShaderOption('C 2_1', 'c2_1', -1.06, 0.883),
-  createShaderOption('C 2_2', 'c2_2', 1.41, 2.259),
-  createShaderOption('LE90', undefined, 1.959, 1.201),
-  createShaderOption('CE90', undefined, 0.531, 0.689),
-];
+}[]> = computed(() => {
+  // TODO: calculate these manually once Cesium supports it. For now, just hardcode
+  // the values for the datasets we care about.
+  switch (props.tiles3d[props.tiles3dId].source.name) {
+    case 'ucsd-all-region':
+      return [
+        createShaderOption('Default', undefined, 0.0, 1.0),
+        createShaderOption('C 0_0', 'c0_0', 0.043, 0.181),
+        createShaderOption('C 1_0', 'c1_0', -0.036, 0.196),
+        createShaderOption('C 1_1', 'c1_1', 0.074, 0.324),
+        createShaderOption('C 2_0', 'c2_0', -0.072, 0.897),
+        createShaderOption('C 2_1', 'c2_1', -1.06, 0.883),
+        createShaderOption('C 2_2', 'c2_2', 1.41, 2.259),
+        createShaderOption('LE90', undefined, 1.959, 1.201),
+        createShaderOption('CE90', undefined, 0.531, 0.689),
+      ];
+    case 'ucsd-full-region-self-error':
+      return [
+        createShaderOption('Default', undefined, 0.0, 1.0),
+        createShaderOption('C 0_0', 'c0_0', 0.08187296986579895, 2.7708709239959717e-05),
+        createShaderOption('C 1_0', 'c1_0', -1.1274002645222936e-05, 8.595945473643951e-06),
+        createShaderOption('C 1_1', 'c1_1', 0.08102615922689438, 1.3262033462524414e-05),
+        createShaderOption('C 2_0', 'c2_0', 0.005069461185485125, 0.00010266946628689766),
+        createShaderOption('C 2_1', 'c2_1', -0.008007237687706947, 8.565373718738556e-06),
+        createShaderOption('C 2_2', 'c2_2', 0.16099649667739868, 0.0001596212387084961),
+        createShaderOption('LE90', undefined, 0.6620119598393063, 0.0003280971701203894),
+        createShaderOption('CE90', undefined, 0.5989373493306599, 0.00010082366752983685),
+      ];
+    case 'ucsd-all-total-error':
+      return [
+        createShaderOption('Default', undefined, 0.0, 1.0),
+        createShaderOption('C 0_0', 'c0_0', 0.12492009997367859, 0.18175694346427917),
+        createShaderOption('C 1_0', 'c1_0', -0.03651577606797218, 0.19667799770832062),
+        createShaderOption('C 1_1', 'c1_1', 0.1556130051612854, 0.3247891366481781),
+        createShaderOption('C 2_0', 'c2_0', -0.7162777185440063, 0.8980929851531982),
+        createShaderOption('C 2_1', 'c2_1', -1.0689252614974976, 0.8836731910705566),
+        createShaderOption('C 2_2', 'c2_2', 1.571554183959961, 2.259568214416504),
+        createShaderOption('LE90', undefined, 2.068341767857969, 1.1610492734220559),
+        createShaderOption('CE90', undefined, 0.7899198029969414, 0.5694263650359508),
+      ];
+    default:
+      return [];
+  }
+});
 
 watchEffect(() => {
   const current = props.tiles3d[props.tiles3dId];
@@ -81,7 +115,7 @@ watchEffect(() => {
     [colormap.value] = colorMapOptions.value;
   }
 
-  const selectedShader = shaderOptions.find((s) => s.title === shader.value);
+  const selectedShader = shaderOptions.value.find((s) => s.title === shader.value);
 
   if (!selectedShader) {
     throw Error('Shader not found!!');
